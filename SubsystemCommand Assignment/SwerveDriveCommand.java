bda17512f6b283e866884a89262d5cb879db070c
Jaeshii <yinj5927@gmail.com>
2019-11-01 01:51:53 -0500

    first commit

---
+++ SubsystemCommand Assignment/SwerveDriveCommand.java
@@ -0,0 +1,56 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+/**
+ * An example command. You can replace me with your own command.
+ */
+public class SwerveDriveCommand extends Command {
+
+  private SwerveSubsystem m_subsystem;
+
+  public SwerveDriveCommand(SwerveSubsystem subsystem) {
+    // Use requires() here to declare subsystem dependencies
+    requires(subsystem);
+
+    m_subsystem = subsystem;
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    DriverStation.reportWarning("Initializing", false);
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    drive(OI.xboxController.getRawAxis(OI.LEFT_STICK_X), OI.xboxController.getRawAxis(OI.LEFT_STICK_Y),
+        OI.xboxController.getRawAxis(OI.RIGHT_STICK_X));
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
